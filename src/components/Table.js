fd6e4330b3ee45d415ad1985456acfbff9a1f465
tavariscodes <tavariscodes@gmail.com>
2021-08-19 17:25:48 -0400

    feat(table): create table component & import to app.js
    Created table component and imported into main App.js file
    for rendering thru App Component

---
+++ src/components/Table.js
@@ -0,0 +1,14 @@
+import React, {Component} from "react";
+import '../css/Table.css';
+
+class Table extends Component {
+    render() {
+        return(
+            <div className="table-container">
+                {/* fill in table elements here */}
+            </div>
+        )
+    }
+}
+
+export default Table;
